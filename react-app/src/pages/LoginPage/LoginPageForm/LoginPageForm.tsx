3d108d953a585f610341658b66512ab77dfb19f1
lmuratore <ltm157m@gmail.com>
2023-03-01 14:13:55 -0500

    Fixed login page and restyled file

--- react-app/src/pages/LoginPage/LoginPageForm/LoginPageForm.tsx
+++ react-app/src/pages/LoginPage/LoginPageForm/LoginPageForm.tsx
@@ -1,15 +1,22 @@
-import styles from './LoginPageForm.module.css'
-import yknotlogo from '../../../assets/yknot-logo.png'
+import styles from './LoginPageForm.module.css';
 
 const LoginPageForm = () => {
-    return <div>
-        <div>{yknotlogo}</div>
-        <h1>Sign In</h1>
-        <input>Email</input>
-        <input>Passoword</input>
-        <p>Forgot Password?</p>
-        <button>Sign In</button>
+  return (
+    <div>
+      <img
+        className={styles.yknotlogo}
+        src={require('../../../assets/yknot-logo.png')}
+        alt="y-knot logo"
+      />
+      <h1>Sign In</h1>
+      <div className={styles.inputBox}>
+        <input type="text" placeholder="Email"></input>
+        <input type="text" placeholder="Password"></input>
+      </div>
+      <p>Forgot Password?</p>
+      <button>Sign In</button>
     </div>
-}
+  );
+};
 
 export default LoginPageForm;
