819b1813e42b097ce86b0fddad42266dd698ce21
Your Name <jnguyengg@gmail.com>
2023-03-11 19:28:01 -0500

    Added types of courses sections and styling

--- react-app/src/pages/CoursesPage/CoursesPage.tsx
+++ react-app/src/pages/CoursesPage/CoursesPage.tsx
@@ -1,10 +1,37 @@
 import CourseCard from '../../components/CourseCard/CourseCard';
+import styles from './CoursesPage.module.css';
 
 const CoursesPage = (): JSX.Element => {
   return (
-    <div>
-      courses
-      <CourseCard teacher="bob" course="1" section="1" />
+    <div className={styles.rightPane}>
+
+      <h1 className={styles.courseStatus}>Active Courses</h1>
+      <div className={styles.cardLayout}>
+        <CourseCard teacher="bob" course="1" section="1"/>
+        <CourseCard teacher="bob" course="1" section="1" />
+        <CourseCard teacher="bob" course="1" section="1" />
+      </div>
+      <div className={styles.cardLayout}>
+        <CourseCard teacher="bob" course="1" section="1" />
+        <CourseCard teacher="bob" course="1" section="1" />
+    </div>
+
+      <h1 className={styles.courseStatus}>Past Courses</h1>
+      <div className={styles.cardLayout}>
+        <CourseCard teacher="bob" course="1" section="1"/>
+        <CourseCard teacher="bob" course="1" section="1" />
+        <CourseCard teacher="bob" course="1" section="1" />
+      </div>
+      <div className={styles.cardLayout}>
+        <CourseCard teacher="bob" course="1" section="1" />
+        <CourseCard teacher="bob" course="1" section="1" />
+      </div>
+
+      <h1 className={styles.courseStatus}>Upcoming Courses</h1>
+      <div className={styles.cardLayout}>
+        <CourseCard teacher="bob" course="1" section="1" />
+        <CourseCard teacher="bob" course="1" section="1" />
+      </div>
     </div>
   );
 };
