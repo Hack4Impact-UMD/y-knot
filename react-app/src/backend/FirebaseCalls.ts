5118ef660800e62a895df3fbddef58225d8cc8d5
sophtsai <sophietsai31@gmail.com>
2023-03-13 22:18:50 -0400

    Add firebase cals: authenticate user, log out, and send reset email

---
+++ react-app/src/backend/FirebaseCalls.ts
@@ -0,0 +1,50 @@
+import { AuthError, User } from '@firebase/auth';
+import {
+  signInWithEmailAndPassword,
+  getAuth,
+  signOut,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
+import app from '../config/firebase';
+
+export function authenticateUser(
+  email: string,
+  password: string,
+): Promise<User> {
+  return new Promise((resolve, reject) => {
+    const auth = getAuth(app);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        resolve(userCredential.user);
+      })
+      .catch((error: AuthError) => {
+        reject(error);
+      });
+  });
+}
+
+export function logOut(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const auth = getAuth(app);
+    signOut(auth)
+      .then(() => {
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
+
+export function sendResetEmail(email: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    const auth = getAuth(app);
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
