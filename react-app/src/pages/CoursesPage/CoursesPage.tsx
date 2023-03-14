535b29755642e570524827db8110a497b5364e87
Sahil Gaba <sgaba9778@gmail.com>
2023-03-13 20:04:20 -0400

    Fixing preview PR live

--- react-app/src/pages/CoursesPage/CoursesPage.tsx
+++ react-app/src/pages/CoursesPage/CoursesPage.tsx
@@ -4,21 +4,20 @@ import styles from './CoursesPage.module.css';
 const CoursesPage = (): JSX.Element => {
   return (
     <div className={styles.rightPane}>
-
       <h1 className={styles.courseStatus}>Active Courses</h1>
       <div className={styles.cardLayout}>
-        <CourseCard teacher="bob" course="1" section="1"/>
+        <CourseCard teacher="bob" course="1" section="1" />
         <CourseCard teacher="bob" course="1" section="1" />
         <CourseCard teacher="bob" course="1" section="1" />
       </div>
       <div className={styles.cardLayout}>
         <CourseCard teacher="bob" course="1" section="1" />
         <CourseCard teacher="bob" course="1" section="1" />
-    </div>
+      </div>
 
       <h1 className={styles.courseStatus}>Past Courses</h1>
       <div className={styles.cardLayout}>
-        <CourseCard teacher="bob" course="1" section="1"/>
+        <CourseCard teacher="bob" course="1" section="1" />
         <CourseCard teacher="bob" course="1" section="1" />
         <CourseCard teacher="bob" course="1" section="1" />
       </div>
