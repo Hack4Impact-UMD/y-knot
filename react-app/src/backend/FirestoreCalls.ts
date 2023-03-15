8e5e0374195f727560460980e8b35d93e3ee923f
lmuratore <ltm157m@gmail.com>
2023-03-15 11:16:42 -0400

    Added addStudent and deleteStudent firebase functions

--- react-app/src/backend/FirestoreCalls.ts
+++ react-app/src/backend/FirestoreCalls.ts
@@ -1,15 +1,41 @@
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, doc, addDoc, deleteDoc } from 'firebase/firestore';
 import { db } from '../config/firebase';
+import { type Student } from '../types/StudentType';
 
 // Sample function
-export function sampleFunction(object: Object): Promise<string> {
-  return new Promise((resolve, reject) => {
+export async function sampleFunction(object: Object): Promise<string> {
+  return await new Promise((resolve, reject) => {
     addDoc(collection(db, 'CollectionName'), object)
       .then((docRef) => {
-        return resolve(docRef.id);
+        resolve(docRef.id);
       })
       .catch((e) => {
-        return reject(e);
+        reject(e);
+      });
+  });
+}
+
+export async function addStudent(student: Student): Promise<string> {
+  return await new Promise((resolve, reject) => {
+    addDoc(collection(db, 'Students'), student)
+      .then((docRef) => {
+        // return id of student added
+        resolve(docRef.id);
+      })
+      .catch((e) => {
+        reject(e);
+      });
+  });
+}
+
+export async function deleteStudent(id: string): Promise<void> {
+  await new Promise((resolve, reject) => {
+    deleteDoc(doc(db, 'Students', id))
+      .then((docRef) => {
+        resolve(docRef);
+      })
+      .catch((e) => {
+        reject(e);
       });
   });
 }
