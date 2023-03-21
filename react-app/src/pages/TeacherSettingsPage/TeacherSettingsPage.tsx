657bd33a822ff4839913e60b1f9b66d5831efa13
Akash <akkusp@outlook.com>
2023-03-20 20:46:28 -0400

    added 3 more boxes, one of which includes the teacher one. Added change password and add teacher buttons, also, changed edit text to react edit icon. Also, added edit functionality to email input.

--- react-app/src/pages/TeacherSettingsPage/TeacherSettingsPage.tsx
+++ react-app/src/pages/TeacherSettingsPage/TeacherSettingsPage.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import styles from './TeacherSettingsPage.module.css';
+import { MdEdit } from 'react-icons/md';
 
 const TeacherSettingsPage = (): JSX.Element => {
-  const [edit, setEdit] = useState(false);
+  const [editName, setEditName] = useState(false);
   const [name, setName] = useState('Eric Johnson');
+  const [editEmail, setEditEmail] = useState(false);
+  const [email, setEmail] = useState('eric.johnson@gmail.com');
 
   return (
     <div className={styles.settings}>
-      <h1 className={styles.title}>Settings</h1>
       <div className={styles.inputs}>
         <div className={styles.box} id="Name">
           <a className={styles.boxTitle}>Name</a>
           <a className={styles.boxData}>
-            {edit ? (
+            {editName ? (
               <input
                 onChange={(event) => {
                   setName(event.target.value);
@@ -25,26 +27,52 @@ const TeacherSettingsPage = (): JSX.Element => {
           <button
             className={styles.editKey}
             onClick={() => {
-              setEdit(!edit);
+              setEditName(!editName);
             }}
           >
-            {edit ? 'save' : 'edit'}
+            {editName ? 'save' : <MdEdit />}
           </button>
         </div>
         <div className={styles.box} id="Email">
           <a className={styles.boxTitle}>Email</a>
-          <a className={styles.boxData}>eric.johnson@gmail.com</a>
-          <button className={styles.editKey}>edit</button>
+          <a className={styles.boxData}>
+            {editEmail ? (
+              <input
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              ></input>
+            ) : (
+              email
+            )}
+          </a>
+          <button
+            className={styles.editKey}
+            onClick={() => {
+              setEditEmail(!editEmail);
+            }}
+          >
+            {editEmail ? 'save' : <MdEdit />}
+          </button>
         </div>
-        <div className={styles.bottomBox} id="Password">
+        <div className={styles.box} id="Password">
           <a className={styles.boxTitle}>Password</a>
           <a className={styles.boxData}>******************</a>
-          <button className={styles.editKey}>edit</button>
+          <button className={styles.editButton}>Change Password</button>
         </div>
-      </div>
-      <div className={styles.buttons}>
-        <button className={styles.button}>Add Teacher</button>
-        <button className={styles.button}>Delete Teacher</button>
+        <div className={styles.box} id="Password">
+          <a className={styles.boxTitle}>Teacher</a>
+          <a className={styles.boxData}></a>
+          <button className={styles.editButton}>Add Teacher</button>
+        </div>
+        <div className={styles.box}>
+          <a className={styles.boxTitle}></a>
+          <a className={styles.boxData}></a>
+        </div>
+        <div className={styles.bottomBox}>
+          <a className={styles.boxTitle}></a>
+          <a className={styles.boxData}></a>
+        </div>
       </div>
     </div>
   );
