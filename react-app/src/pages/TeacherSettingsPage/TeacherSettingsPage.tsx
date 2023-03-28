9f2615a07a0f74ebf5b22f4cfcd983fa7fb916ad
Cole Thrailkill <cthrailk@terpmail.umd.edu>
2023-03-28 16:31:48 -0400

    Connect email/password modals to teacher settings

--- react-app/src/pages/TeacherSettingsPage/TeacherSettingsPage.tsx
+++ react-app/src/pages/TeacherSettingsPage/TeacherSettingsPage.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import styles from './TeacherSettingsPage.module.css';
 import { MdEdit } from 'react-icons/md';
+import ResetEmail from '../../components/ResetEmail/ResetEmail';
+import ResetPassword from '../../components/ResetPassword/ResetPassword';
 
 const TeacherSettingsPage = (): JSX.Element => {
   const [editName, setEditName] = useState(false);
   const [name, setName] = useState('Eric Johnson');
-  const [editEmail, setEditEmail] = useState(false);
+  //const [editEmail, setEditEmail] = useState(false);
   const [email, setEmail] = useState('eric.johnson@gmail.com');
+  const [openEmailModal, setOpenEmailModal] = useState<boolean>(false);
+  const [openPasswordModal, setOpenPasswordModal] = useState<boolean>(false);
 
   return (
     <div className={styles.settings}>
@@ -35,7 +39,18 @@ const TeacherSettingsPage = (): JSX.Element => {
         </div>
         <div className={styles.box} id="Email">
           <a className={styles.boxTitle}>Email</a>
-          <a className={styles.boxData}>
+          <a className={styles.boxData}>{email}</a>
+          <button
+            className={styles.editKey}
+            onClick={() => setOpenEmailModal(!openEmailModal)}
+          >
+            <MdEdit />
+          </button>
+          <ResetEmail
+            open={openEmailModal}
+            onClose={() => setOpenEmailModal(!openEmailModal)}
+          />
+          {/*<a className={styles.boxData}>
             {editEmail ? (
               <input
                 onChange={(event) => {
@@ -45,7 +60,7 @@ const TeacherSettingsPage = (): JSX.Element => {
             ) : (
               email
             )}
-          </a>
+            </a>
           <button
             className={styles.editKey}
             onClick={() => {
@@ -53,12 +68,21 @@ const TeacherSettingsPage = (): JSX.Element => {
             }}
           >
             {editEmail ? 'save' : <MdEdit />}
-          </button>
+          </button>*/}
         </div>
         <div className={styles.box} id="Password">
           <a className={styles.boxTitle}>Password</a>
           <a className={styles.boxData}>******************</a>
-          <button className={styles.editButton}>Change Password</button>
+          <button
+            className={styles.editButton}
+            onClick={() => setOpenPasswordModal(!openPasswordModal)}
+          >
+            Change Password
+          </button>
+          <ResetPassword
+            open={openPasswordModal}
+            onClose={() => setOpenPasswordModal(!openPasswordModal)}
+          />
         </div>
         <div className={styles.box} id="Password">
           <a className={styles.boxTitle}>Teacher</a>
