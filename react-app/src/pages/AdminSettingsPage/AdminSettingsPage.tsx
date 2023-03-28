d6090b4535e1f7e4d60a572a1ad8ce9718280d91
Cole Thrailkill <cthrailk@terpmail.umd.edu>
2023-03-28 15:43:18 -0400

    Connect reset modals to admin settings

--- react-app/src/pages/AdminSettingsPage/AdminSettingsPage.tsx
+++ react-app/src/pages/AdminSettingsPage/AdminSettingsPage.tsx
@@ -1,9 +1,13 @@
 import styles from './AdminSettingsPage.module.css';
 import React, { useState } from 'react';
+import ResetEmail from '../../components/ResetEmail/ResetEmail';
+import ResetPassword from '../../components/ResetPassword/ResetPassword';
 
 const AdminSettingsPage = (): JSX.Element => {
   const [edit, setEdit] = useState(false);
   const [name, setName] = useState('Eric Johnson');
+  const [openEmailModal, setOpenEmailModal] = useState<boolean>(false);
+  const [openPasswordModal, setOpenPasswordModal] = useState<boolean>(false);
 
   return (
     <div className={styles.settings}>
@@ -34,12 +38,30 @@ const AdminSettingsPage = (): JSX.Element => {
         <div className={styles.box} id="Email">
           <a className={styles.boxTitle}>Email</a>
           <a className={styles.boxData}>eric.johnson@gmail.com</a>
-          <button className={styles.editKey}>edit</button>
+          <button
+            className={styles.editKey}
+            onClick={() => setOpenEmailModal(!openEmailModal)}
+          >
+            edit
+          </button>
+          <ResetEmail
+            open={openEmailModal}
+            onClose={() => setOpenEmailModal(!openEmailModal)}
+          />
         </div>
         <div className={styles.bottomBox} id="Password">
           <a className={styles.boxTitle}>Password</a>
           <a className={styles.boxData}>******************</a>
-          <button className={styles.editKey}>edit</button>
+          <button
+            className={styles.editKey}
+            onClick={() => setOpenPasswordModal(!openPasswordModal)}
+          >
+            edit
+          </button>
+          <ResetPassword
+            open={openPasswordModal}
+            onClose={() => setOpenPasswordModal(!openPasswordModal)}
+          />
         </div>
       </div>
       <div className={styles.buttons}>
