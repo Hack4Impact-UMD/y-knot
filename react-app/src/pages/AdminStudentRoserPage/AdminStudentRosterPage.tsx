9a30778be4c398a97f8b05ed586972e7a4352005
Akash <akkusp@outlook.com>
2023-04-04 19:50:39 -0400

    comsrhihng

--- react-app/src/pages/AdminStudentRoserPage/AdminStudentRosterPage.tsx
+++ react-app/src/pages/AdminStudentRoserPage/AdminStudentRosterPage.tsx
@@ -1,18 +1,54 @@
 import styles from './AdminStudentRosterPage.module.css';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
 import StudentList from './StudentList/StudentList';
+import { Student } from '../../types/StudentType';
+import { useState, useEffect } from 'react';
+import { getAllStudents } from '../../backend/FirestoreCalls';
+import { authenticateUser } from '../../backend/FirebaseCalls';
 
 const AdminStudentRosterPage = (): JSX.Element => {
-    return (
-    <div className={styles.rightPane}>
-         <NavigationBar />
-         <div className={styles.searchBar}>
-            <input type="text" placeholder="Search Students" />
-         </div>
-         <h1 className={styles.heading}>Student Roster</h1>
-         <StudentList />
-    </div>
+  const [searchName, setSearchName] = useState<String>();
+  const [students, setStudents] = useState<Student[]>([]);
+  const [tempRoster, setTempRoster] = useState<Student[]>([]);
+
+  useEffect(() => {
+    authenticateUser('sgaba@umd.edu', '123abc')
+      .then(() =>
+        getAllStudents()
+          .then((data) => setStudents(data))
+          .catch((err) => console.error(err)),
+      )
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  const handleSearch = (e: any) => {
+    console.log(e.target.value);
+    setSearchName(e.target.value);
+    setTempRoster(students);
+    tempRoster.filter(
+      (student) =>
+        student.firstName === searchName || student.lastName === searchName,
     );
   };
-  
-  export default AdminStudentRosterPage;
+
+  return (
+    <div className={styles.rightPane}>
+      <NavigationBar />
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          placeholder="Search Students"
+          onChange={(e) => {
+            handleSearch(e);
+          }}
+        />
+      </div>
+      <h1 className={styles.heading}>Student Roster</h1>
+      <StudentList students={tempRoster} />
+    </div>
+  );
+};
+
+export default AdminStudentRosterPage;
--- react-app/src/pages/AdminStudentRoserPage/StudentList/StudentList.tsx
+++ react-app/src/pages/AdminStudentRoserPage/StudentList/StudentList.tsx
@@ -1,30 +1,10 @@
-import { useEffect, useState } from 'react';
-import { getAllStudents } from '../../../backend/FirestoreCalls';
 import { FaTrash } from 'react-icons/fa';
 import { Student } from '../../../types/StudentType';
 import { ReactComponent as EyeIcon } from '../../../assets/eye.svg';
 import styles from './StudentList.module.css';
-import { authenticateUser } from '../../../backend/FirebaseCalls';
 
-const StudentList = () => {
-  const [students, setStudents] = useState<Student[]>([]);
-
-  useEffect(() => {
-    authenticateUser('sgaba@umd.edu', '123abc')
-      .then(() =>
-        getAllStudents()
-          .then((data) =>
-            setStudents(data)
-          )
-          .catch((err) =>
-            console.error(err)
-          )
-      ).catch((err) => {
-        console.error(err);
-      });
-
-    console.log(students);
-  }, []);
+const StudentList = (props: { students: Student[] }) => {
+  const { students } = props;
 
   return (
     <div>
