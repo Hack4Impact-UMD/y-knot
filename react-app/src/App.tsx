48cc286941497b2537174dc2e08a7e71b9e3a19d
sophtsai <sophietsai31@gmail.com>
2023-04-11 16:16:05 -0400

    Show Admin/Teacher view depending on role

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -6,12 +6,12 @@ import Sample404Page from './pages/Sample404Page/Sample404Page';
 import LoginPage from './pages/LoginPage/LoginPage';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import CoursesPage from './pages/CoursesPage/CoursesPage';
+import ClassPage from './pages/ClassPage/ClassPage';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './muiTheme';
 import { addSampleStudent } from './backendTesting/test';
 import { authenticateUser } from './backend/FirebaseCalls';
 import { Upload } from './components/Upload/Upload';
-import AdminClassPage from './pages/ClassPage/AdminClassPage';
 
 function App(): JSX.Element {
   const customTheme = theme;
@@ -56,7 +56,7 @@ function App(): JSX.Element {
               }
             />
             <Route path="/upload" element={<Upload />} />
-            <Route path="/class" element={<AdminClassPage />} />
+            <Route path="/class" element={<ClassPage />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
