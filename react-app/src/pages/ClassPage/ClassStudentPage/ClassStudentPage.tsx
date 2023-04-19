3993b3353d80499002316e76629a795fd4b02cac
Cole Thrailkill <cthrailk@terpmail.umd.edu>
2023-04-18 21:56:46 -0400

    Class students page

---
+++ react-app/src/pages/ClassPage/ClassStudentPage/ClassStudentPage.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import styles from './ClassStudentPage.module.css';
+import { useAuth } from '../../../auth/AuthProvider';
+import Loading from '../../../components/LoadingScreen/Loading';
+
+import CertificateIcon from '../../../assets/certificate.svg';
+import EyeIcon from '../../../assets/view.svg';
+import TrashIcon from '../../../assets/trash.svg';
+
+interface StudentInfo {
+  name: string;
+  email: string;
+}
+
+const ClassStudentPage = (): JSX.Element => {
+  const [students, setStudents] = useState<StudentInfo[]>([
+    { name: 'Fiona Love', email: 'f.love@gmail.com' },
+    { name: 'Alicia Jacobs', email: 'a.jacobs@gmail.com' },
+    { name: 'Emily Lee', email: 'e.lee@gmail.com' },
+    { name: 'Brian Bailey', email: 'b.bailey@gmail.com' },
+    { name: 'Ariana Apple', email: 'a.apple@gmail.com' },
+    { name: 'Chloe Martinez', email: 'c.martinez@gmail.com' },
+  ]);
+
+  const authContext = useAuth();
+
+  return (
+    <>
+      {authContext?.loading ? (
+        <div className={styles.loadingContainer}>
+          <Loading />
+        </div>
+      ) : (
+        <div className={styles.studentsContainer}>
+          {students.map((student, i) => {
+            let roundTop = i === 0 ? styles.roundTop : '';
+            let roundBottom =
+              i === students.length - 1 ? styles.roundBottom : '';
+
+            return (
+              <div
+                key={i}
+                className={`${styles.box} ${roundTop} ${roundBottom}`}
+              >
+                <div className={styles.studentName}>
+                  <p>{student.name}</p>
+                </div>
+                <div className={styles.studentEmail}>
+                  <p>{student.email}</p>
+                </div>
+                <div className={styles.icons}>
+                  <img src={CertificateIcon} />
+                  {authContext?.token?.claims.role === 'admin' && (
+                    <img src={EyeIcon} />
+                  )}
+                  <img src={TrashIcon} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ClassStudentPage;
