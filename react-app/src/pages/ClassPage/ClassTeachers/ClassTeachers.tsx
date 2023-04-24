90eb587375af140e5e78d27367eeec4b46cfdb68
sophtsai <sophietsai31@gmail.com>
2023-04-24 17:33:36 -0400

    Clean id and class naming

--- react-app/src/pages/ClassPage/ClassTeachers/ClassTeachers.tsx
+++ react-app/src/pages/ClassPage/ClassTeachers/ClassTeachers.tsx
@@ -5,48 +5,32 @@ const ClassTeachers = (): JSX.Element => {
   return (
     <div className={styles.container}>
       <div className={styles.inputs}>
-        <div className={styles.box} id="Name">
+        <div className={styles.box}>
           <a className={styles.boxTitle}>Fiona Love</a>
           <div className={styles.icons}>
-            <img
-              src={eyeIcon}
-              className={`${styles.eyeIcon} eyeIcon`}
-              id="icon"
-            ></img>
-            <img src={trashIcon} className="trashIcon" id="icon"></img>
+            <img src={eyeIcon}></img>
+            <img src={trashIcon}></img>
           </div>
         </div>
-        <div className={styles.box} id="Name">
+        <div className={styles.box}>
           <a className={styles.boxTitle}>Alicia Jacobs</a>
           <div className={styles.icons}>
-            <img
-              src={eyeIcon}
-              className={`${styles.eyeIcon} eyeIcon`}
-              id="icon"
-            ></img>
-            <img src={trashIcon} className="trashIcon" id="icon"></img>
+            <img src={eyeIcon}></img>
+            <img src={trashIcon}></img>
           </div>
         </div>
-        <div className={styles.box} id="Name">
+        <div className={styles.box}>
           <a className={styles.boxTitle}>Emily Lee</a>
           <div className={styles.icons}>
-            <img
-              src={eyeIcon}
-              className={`${styles.eyeIcon} eyeIcon`}
-              id="icon"
-            ></img>
-            <img src={trashIcon} className="trashIcon" id="icon"></img>
+            <img src={eyeIcon}></img>
+            <img src={trashIcon}></img>
           </div>
         </div>
-        <div className={styles.box} id="Name">
+        <div className={styles.box}>
           <a className={styles.boxTitle}>Brian Bailey</a>
           <div className={styles.icons}>
-            <img
-              src={eyeIcon}
-              className={`${styles.eyeIcon} eyeIcon`}
-              id="icon"
-            ></img>
-            <img src={trashIcon} className="trashIcon" id="icon"></img>
+            <img src={eyeIcon}></img>
+            <img src={trashIcon}></img>
           </div>
         </div>
       </div>
