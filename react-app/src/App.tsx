60442f5ba3cddd3846226a710018d1acb04d0140
sophtsai <sophietsai31@gmail.com>
2023-11-08 01:31:02 -0500

    Added function to add teacher to course, WIP

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,7 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './auth/AuthProvider';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './muiTheme';
-import { addSampleCourse, addSampleStudent, addStudentInCourse } from './backendTesting/test';
+import {
+  addSampleCourse,
+  addSampleStudent,
+  addStudentInCourse,
+} from './backendTesting/test';
 import RequireAuth from './auth/RequireAuth/RequireAuth';
 import RequireAdminAuth from './auth/RequireAdminAuth/RequireAdminAuth';
 import LoginPage from './pages/LoginPage/LoginPage';
@@ -18,7 +22,7 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import NavigationBar from './components/NavigationBar/NavigationBar';
 import TeacherRosterPage from './pages/TeacherRosterPage/TeacherRosterPage';
 import { createUser } from './backend/CloudFunctionsCalls';
-import { addCourse } from './backend/FirestoreCalls';
+import { addCourse, addTeacherCourse } from './backend/FirestoreCalls';
 
 function App(): JSX.Element {
   const customTheme = theme;
@@ -97,23 +101,31 @@ function App(): JSX.Element {
               element={
                 <RequireAuth>
                   <button
-                    onClick={async () => {
-                      const course = addCourse({
-                        name: 'Digital Marketing',
-                        startDate: '2022-08-14',
-                        endDate: '2023-10-30',
-                        students: [],
-                        courseType: 'PROGRAM',
-                        teachers: [],
-                        meetingTime: 'Tuesdays 9:30AM',
-                        leadershipApp: false,
-                        formId: '',
-                        introEmail: { content: 'this is an intro email.' },
-                        attendance: [],
-                        homeworks: []
-                      })
-                      addStudentInCourse(await course);
-                    }}
+                    onClick={
+                      () => {
+                        addTeacherCourse(
+                          'Li3x7GK5XwAk8UqhskKV',
+                          'tL77SdWoQF3y9F6duXnN',
+                        );
+                      }
+                      //   async () => {
+                      //   const course = addCourse({
+                      //     name: 'Digital Marketing',
+                      //     startDate: '2022-08-14',
+                      //     endDate: '2023-10-30',
+                      //     students: [],
+                      //     courseType: 'PROGRAM',
+                      //     teachers: [],
+                      //     meetingTime: 'Tuesdays 9:30AM',
+                      //     leadershipApp: false,
+                      //     formId: '',
+                      //     introEmail: { content: 'this is an intro email.' },
+                      //     attendance: [],
+                      //     homeworks: []
+                      //   })
+                      //   addStudentInCourse(await course);
+                      // }
+                    }
                   ></button>
                 </RequireAuth>
               }
--- react-app/src/backend/FirestoreCalls.ts
+++ react-app/src/backend/FirestoreCalls.ts
@@ -127,6 +127,41 @@ export function deleteStudent(id: string): Promise<void> {
   });
 }
 
+export function addTeacherCourse(
+  teacherId: string,
+  courseId: string,
+): Promise<void> {
+  return new Promise((resolve, reject) => {
+    /* runTransaction provides protection against race conditions where
+       2 people are modifying the data at once. It also ensures that either
+       all of these writes succeed or none of them do.
+    */
+    runTransaction(db, async (transaction) => {
+      const teacherRef = await transaction.get(doc(db, 'Users', teacherId));
+      const courseRef = await transaction.get(doc(db, 'Courses', courseId));
+      if (!teacherRef.exists() || !courseRef.exists()) {
+        throw 'Document does not exist!';
+      }
+      const teacher: Teacher = teacherRef.data() as Teacher;
+      teacher.courses.push(courseId);
+      await transaction.update(doc(db, 'Users', teacherId), {
+        courses: teacher.courses,
+      });
+      const course: Course = courseRef.data() as Course;
+      course.teachers.push(teacherId);
+      await transaction.update(doc(db, 'Courses', courseId), {
+        teachers: course.teachers,
+      });
+    })
+      .then(() => {
+        resolve();
+      })
+      .catch(() => {
+        reject();
+      });
+  });
+}
+
 export function addCourse(course: Course): Promise<string> {
   return new Promise((resolve, reject) => {
     addDoc(collection(db, 'Courses'), course)
