f12912ce44199620c98456357ca3e11813517346
ytol <106978109+ytol@users.noreply.github.com>
2024-01-01 19:52:39 -0500

    updated styling, added image, added date picker

--- react-app/src/pages/ClassPage/ClassSettings/ClassSettings.tsx
+++ react-app/src/pages/ClassPage/ClassSettings/ClassSettings.tsx
@@ -6,6 +6,7 @@ import Select from 'react-select';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import someIcon from '../../../assets/edit.svg';
 
 const ClassPage = (): JSX.Element => {
   const [fieldErrors, setFieldErrors] = useState<Record<string, string>>({});
@@ -13,107 +14,111 @@ const ClassPage = (): JSX.Element => {
   const dropdownOptions = ['Program', 'Academy', 'Club'];
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.row}>
-        <p className={styles.headerText}> Course Name </p>
-        <input
-          type="text"
-          placeholder="Enter Name"
-          className={styles.courseInput}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-
-      <div className={styles.row}>
-        <p className={styles.headerText}> Start Date </p>
-        <div className={styles.calendar}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label=""
-              slotProps={{ textField: { size: 'small' } }}
-              sx={{
-                '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-                  border: '1px solid black',
-                }, // at page load
-                '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
-                  { border: '2px solid black' }, // at focused state
-              }}
-            />{' '}
-          </LocalizationProvider>
-        </div>
-      </div>
-
-      <div className={styles.row}>
-        <p className={styles.headerText}> End Date </p>
-        <div className={styles.calendar}>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              label=""
-              slotProps={{ textField: { size: 'small' } }}
-              sx={{
-                '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-                  border: '1px solid gray',
-                }, // at page load
-                '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
-                  { border: '1px solid gray' }, // at focused state
-              }}
-            />{' '}
-          </LocalizationProvider>
-        </div>
-      </div>
-
-      <div className={styles.row}>
-        <p className={styles.headerText}> Course Type </p>
-        <Select
-          placeholder="Select Program"
-          styles={{
-            control: (baseStyles) => ({
-              ...baseStyles,
-              borderColor: 'black',
-              width: '250px',
-              height: '50px',
-            }),
-          }}
-          options={dropdownOptions.map((option) => {
-            return { value: option, label: option };
-          })}
-        />
-      </div>
-
-      <div className={styles.row}>
-        <p className={styles.headerText}> Leadership Application </p>
-        <div className={styles.buttonContainer}>
+      <div className={styles.topContainer}>
+        <div className={styles.row}>
+          <p className={styles.headerText}> Course Name </p>
           <input
-            className={styles.button}
-            id="Yes"
-            type="radio"
-            name="radioGroup"
-            value="yes"
-            checked
+            type="text"
+            placeholder="Enter Name"
+            className={styles.courseInput}
+            onChange={(event) => setName(event.target.value)}
           />
-          <label className={styles.yesLabel} htmlFor="Yes">
-            Yes
-          </label>
+        </div>
+        <div className={styles.row}>
+          <p className={styles.headerText}> Start Date </p>
+          <div className={styles.calendar}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label=""
+                slotProps={{ textField: { size: 'small' } }}
+                sx={{
+                  '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+                    border: '1px solid black',
+                  }, // at page load
+                  '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+                    { border: '2px solid black' }, // at focused state
+                }}
+              />{' '}
+            </LocalizationProvider>
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <p className={styles.headerText}> End Date </p>
+          <div className={styles.calendar}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label=""
+                slotProps={{ textField: { size: 'small' } }}
+                sx={{
+                  '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
+                    border: '1px solid gray',
+                  }, // at page load
+                  '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline':
+                    { border: '1px solid gray' }, // at focused state
+                }}
+              />{' '}
+            </LocalizationProvider>
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <p className={styles.headerText}> Course Type </p>
+          <Select
+            placeholder="Select Program"
+            styles={{
+              control: (baseStyles) => ({
+                ...baseStyles,
+                borderColor: 'black',
+                width: '250px',
+                height: '50px',
+              }),
+            }}
+            options={dropdownOptions.map((option) => {
+              return { value: option, label: option };
+            })}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <p className={styles.headerText}> Leadership Application </p>
+          <div className={styles.buttonContainer}>
+            <input
+              className={styles.button}
+              id="Yes"
+              type="radio"
+              name="radioGroup"
+              value="yes"
+              checked
+            />
+            <label className={styles.yesLabel} htmlFor="Yes">
+              Yes
+            </label>
+            <input
+              className={styles.button}
+              id="No"
+              type="radio"
+              name="radioGroup"
+              value="no"
+            />
+            <label className={styles.noLabel} htmlFor="No">
+              No
+            </label>
+          </div>
+        </div>
+
+        <div className={styles.lastRow}>
+          <p className={styles.headerText}> Form ID </p>
           <input
-            className={styles.button}
-            id="No"
-            type="radio"
-            name="radioGroup"
-            value="no"
+            type="text"
+            placeholder="Enter ID"
+            className={styles.formIDInput}
+            onChange={(event) => setName(event.target.value)}
           />
-          <label className={styles.noLabel} htmlFor="No">
-            No
-          </label>
         </div>
       </div>
-
-      <div className={styles.lastRow}>
-        <p className={styles.headerText}> Form ID </p>
-        <input
-          type="text"
-          placeholder="Enter ID"
-          className={styles.formIDInput}
-          onChange={(event) => setName(event.target.value)}
-        />
+      <div className={styles.bottomButton}>
+        <img className={styles.someIcon} src={someIcon}></img>
       </div>
     </div>
   );
