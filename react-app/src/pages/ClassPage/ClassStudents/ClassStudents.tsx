5e10e884be9c9f0feed2533903c2e56dd4e7a63c
sophtsai <sophietsai31@gmail.com>
2024-01-04 02:26:27 -0500

    Clean ClassStudents code

--- react-app/src/pages/ClassPage/ClassStudents/ClassStudents.tsx
+++ react-app/src/pages/ClassPage/ClassStudents/ClassStudents.tsx
@@ -97,53 +97,7 @@ const ClassStudents = (props: {
         <h4 className={styles.noStudent}>No Students Currently in Roster</h4>
       ) : (
         // student roster
-        <div className={styles.studentsContainer}>
-          {studentList}
-          {/* {props.students.map((student, i) => {
-            const roundTop = i === 0 ? styles.roundTop : '';
-            const roundBottom =
-              i === props.students.length - 1 ? styles.roundBottom : '';
-
-            return (
-              <div
-                key={i}
-                className={`${styles.box} ${roundTop} ${roundBottom}`}
-              >
-                <div className={styles.studentName}>
-                  <p>{`${student.firstName} ${student.lastName}`}</p>
-                </div>
-                <div className={styles.studentEmail}>
-                  <p>{student.email}</p>
-                </div>
-                <div className={styles.icons}>
-                  <ToolTip title="Send Certificate" placement="top">
-                    <button className={styles.button}>
-                      <img
-                        src={CertificateIcon}
-                        className={styles.certificateIcon}
-                      />
-                    </button>
-                  </ToolTip>
-                  {authContext?.token?.claims.role === 'ADMIN' && (
-                    // See student profile only if admin
-                    <Link to={`/students/${student.id}`}>
-                      <ToolTip title="View Profile" placement="top">
-                        <button className={styles.button}>
-                          <img src={EyeIcon} className={styles.profileIcon} />
-                        </button>
-                      </ToolTip>
-                    </Link>
-                  )}
-                  <ToolTip title="Remove" placement="top">
-                    <button className={styles.button}>
-                      <img src={TrashIcon} className={styles.trashIcon} />
-                    </button>
-                  </ToolTip>
-                </div>
-              </div>
-            );
-          })} */}
-        </div>
+        <div className={styles.studentsContainer}>{studentList}</div>
       )}
       {showPopup && (
         <DeleteStudentClassConfirmation
